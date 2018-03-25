b92c54325dc89fff8eff525a84de432577e85e27
ben <ben@ben-MS-7982>
2018-03-25 20:51:51 +0200

    Wat abacadabra.

--- src/main/java/smarthome/slimmemeter/service/HeatService.java
+++ src/main/java/smarthome/slimmemeter/service/HeatService.java
@@ -10,39 +10,50 @@ import smarthome.slimmemeter.repository.HeatRepository;
 public class HeatService {
 
 	/*
- 	* Q = m * c * deltaT
- 	* 
- 	* kg/s * kJ/(kg*K) * K = kJ/s = kW.
+	 * Q = m * c * deltaT
+	 * 
+	 * kg/s * kJ/(kg*K) * K = kJ/s = kW.
 	 */
-	
+
 	private static final double FLOW_HOT_WATER = 0.222; // = 800 l/h
 	private static final double FLOW_HEATING = 0.083; // = 300 l/h
-	private static final double SPECIFIC_HEAT = 4.18 ; // kJ/(kg*K)
-	private static final double TRESHOLD_DELTA_TEMP_HEATING_WATER_ON = 30 ; // K
-	private static final double TRESHOLD_DELTA_TEMP_HOT_WATER_ON = 30 ; // K
-	
+	private static final double SPECIFIC_HEAT = 4.18; // kJ/(kg*K)
+	private static final double TRESHOLD_TEMP_HEATING_WATER_ON = 55; // K
+	private static final double TRESHOLD_TEMP_HOT_WATER_ON = 40; // K
+	private static final double TRESHOLD_DELTA_TEMP_EXCHANGER_ON = 35; // K
+
+	private double deltaT;
+
 	@Inject
 	private HeatRepository heatRepo;
 
 	// in kW.
 	public double getHeatingPower() {
-		double deltaTempHeatExchanger = heatRepo.getTempHeatExchangeIn() - heatRepo.getTempHeatExchangeOut();
-		return isHeatingOn() ? FLOW_HEATING * SPECIFIC_HEAT * deltaTempHeatExchanger : 0;		
+		return isHeatingOn() ? FLOW_HEATING * SPECIFIC_HEAT * deltaT : 0;
 	}
-
 
 	// in kW.
 	public double getHotWaterPower() {
-		double deltaTempHeatExchanger = heatRepo.getTempHeatExchangeIn() - heatRepo.getTempHeatExchangeOut();
-		return isHotWaterOn() ? FLOW_HOT_WATER * SPECIFIC_HEAT * deltaTempHeatExchanger : 0;
-	}
-	
-	public boolean isHeatingOn() {
-		return heatRepo.getTempHeatingIn() > TRESHOLD_DELTA_TEMP_HEATING_WATER_ON;		
+		return isHotWaterOn() ? FLOW_HOT_WATER * SPECIFIC_HEAT * deltaT : 0;
 	}
 
-	public boolean isHotWaterOn() {
-		return heatRepo.getTempHotWaterIn() > TRESHOLD_DELTA_TEMP_HOT_WATER_ON;
+	private boolean isExchangerOn() {
+		return getDeltaT() > TRESHOLD_DELTA_TEMP_EXCHANGER_ON;
+	}
+
+
+	private boolean isHeatingOn() {
+		return isExchangerOn()
+				&& heatRepo.getTempHeatingIn() > TRESHOLD_TEMP_HEATING_WATER_ON;
 	}
 	
+	private boolean isHotWaterOn() {
+		return isExchangerOn()
+				&& heatRepo.getTempHotWaterIn() > TRESHOLD_TEMP_HOT_WATER_ON;
+	}
+
+	private double getDeltaT() {
+		deltaT = heatRepo.getTempHeatExchangeIn() - heatRepo.getTempHeatExchangeOut();
+		return deltaT;
+	}
 }
